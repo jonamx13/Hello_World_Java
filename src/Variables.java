f2fd9e7a52c6bfcecd35bdf0c421ce25a0fdea3a
jonamx13 <lav.jonathanom@gmail.com>
2023-12-22 08:07:27 -0600

    "Variables"

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Declaring a variable
+        int speed;
+        //Assigning a value to a variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Jonathan Meixueiro";
+        System.out.println(employeeName);
+    }
+}
