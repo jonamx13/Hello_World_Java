0a5bab16539f4b0f8022cdf3c7328f0bc3fae7c6
jonamx13 <lav.jonathanom@gmail.com>
2023-12-25 15:14:57 -0600

    Comments and Java Docs

--- src/Functions.java
+++ src/Functions.java
@@ -20,18 +20,46 @@ public class Functions {
         System.out.println("COLOMBIAN PESOS TO DOLLARS: " + convertToDollar(1000, "COP"));
     }
 
+    // Single line comment
+
+    /*
+     *  Multi-Line
+     *  Comment
+     */
+
+    /**
+     * Description: This function receives a radius value to calculate the AREA of a CIRCLE
+     * @param r it receives a radius value
+     * @return return the result of 'PI * r2' formula.
+     */
     public static double circleArea(double r) {
         return Math.PI * Math.pow(r, 2);
     }
 
+    /**
+     * Description: This function receives a radius value to calculate the AREA of a SPHERE
+     * @param r it receives a radius value
+     * @return return the result of '4 * PI * r2' formula.
+     */
     public static double sphereArea(double r) {
         return 4 * Math.PI * Math.pow(r,2);
     }
 
+    /**
+     * Description: This function receives a radius value to calculate the VOLUME of a SPHERE
+     * @param r it receives a radius value
+     * @return return the result of '(4/3) * PI * r3' formula.
+     */
     public static double sphereVolume(double r) {
         return (4/3) * Math.PI * Math.pow(r,3);
     }
 
+    /**
+     * Description: This function that specifying currency, converts its quantity into dollars
+     * @param quantity Money quantity
+     * @param currency Type of currency: IT ONLY ACCEPTS 'MXN' & 'COP'
+     * @return param 'quantity' will return equivalence into american dollars
+     */
     public static double convertToDollar(double quantity, String currency) {
         // MXN COP
         switch (currency) {
