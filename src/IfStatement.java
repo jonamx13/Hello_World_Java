6284d43a32902bcbbabadb45ee8c156556546fdd
jonamx13 <lav.jonathanom@gmail.com>
2023-12-25 11:52:35 -0600

    Scope and else statement

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -7,6 +7,17 @@ public class IfStatement {
             // Send file
             fileSent++;
             System.out.println("File sent");
+
+            int i = 0;
+            i++;
+            System.out.println(i);
+        } else {
+            fileSent--;
+            System.out.println("Please turn on Bluetooth");
         }
+
+        System.out.println(isBluetoothEnabled);
+        System.out.println(fileSent);
+        // System.out.println(i); this variable can't be reached due to its scope.
     }
 }
