9df019bd395a9e043c304dc3de161dc4bdd0e695
jonamx13 <lav.jonathanom@gmail.com>
2023-12-25 14:22:30 -0600

    Functions

---
+++ src/Functions.java
@@ -0,0 +1,48 @@
+public class Functions {
+    public static void main(String[] args) {
+        double y = 3;
+        // A circle area
+        // PI * r2
+        double area = circleArea(y);
+        System.out.println(area);
+        System.out.println(circleArea(y));
+
+        // A sphere area
+        // 4 * PI * r2
+        System.out.println(sphereArea(y));
+
+        // A sphere volume
+        // (4/3) * PI * r3
+        System.out.println(sphereVolume(y));
+
+        // Convert currencies
+        System.out.println("MEXICAN PESOS TO DOLLARS: " + convertToDollar(200, "MXN"));
+        System.out.println("COLOMBIAN PESOS TO DOLLARS: " + convertToDollar(1000, "COP"));
+    }
+
+    public static double circleArea(double r) {
+        return Math.PI * Math.pow(r, 2);
+    }
+
+    public static double sphereArea(double r) {
+        return 4 * Math.PI * Math.pow(r,2);
+    }
+
+    public static double sphereVolume(double r) {
+        return (4/3) * Math.PI * Math.pow(r,3);
+    }
+
+    public static double convertToDollar(double quantity, String currency) {
+        // MXN COP
+        switch (currency) {
+            case "MXN":
+                quantity *= 0.059;
+                break;
+            case "COP":
+                quantity *= 0.00025;
+                break;
+        }
+
+        return quantity;
+    }
+}
